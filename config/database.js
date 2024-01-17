54319bec997f329b311bc1211fd0f46c51037ae2
Mo-mchawrab <mohamad@propertyvalue.ae>
2024-01-17 16:19:42 +0200

    First commit to repro

---
+++ config/database.js
@@ -0,0 +1,24 @@
+// strapi-api/config/database.js
+module.exports = ({ env }) => ({
+  connection: {
+    client: "postgres",
+    connection: {
+      connectionString: env("DATABASE_URL"),
+      host: env("DATABASE_HOST", "localhost"),
+      port: env.int("DATABASE_PORT", 5432),
+      database: env("DATABASE_NAME", "property_value"),
+      user: env("DATABASE_USERNAME", "postgres"),
+      password: env("DATABASE_PASSWORD", "pass_PV"),
+      ssl: env.bool("DATABASE_SSL", false) && {
+        key: env("DATABASE_SSL_KEY", undefined),
+        cert: env("DATABASE_SSL_CERT", undefined),
+        ca: env("DATABASE_SSL_CA", undefined),
+        capath: env("DATABASE_SSL_CAPATH", undefined),
+        cipher: env("DATABASE_SSL_CIPHER", undefined),
+        rejectUnauthorized: env.bool("DATABASE_SSL_REJECT_UNAUTHORIZED", true),
+      },
+      schema: env("DATABASE_SCHEMA", "public"),
+    },
+    debug: false,
+  },
+});
