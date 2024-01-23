c5d018087aef057ca6941f8947dad3eeaa485762
Mo-mchawrab <mohamad@propertyvalue.ae>
2024-01-23 22:50:37 +0200

    Adding docker to the project

--- config/database.js
+++ config/database.js
@@ -4,11 +4,11 @@ module.exports = ({ env }) => ({
     client: "postgres",
     connection: {
       connectionString: env("DATABASE_URL"),
-      host: env("DATABASE_HOST", "localhost"),
-      port: env.int("DATABASE_PORT", 5432),
-      database: env("DATABASE_NAME", "property_value"),
-      user: env("DATABASE_USERNAME", "postgres"),
-      password: env("DATABASE_PASSWORD", "pass_PV"),
+      host: env("DATABASE_HOST"),
+      port: env.int("DATABASE_PORT"),
+      database: env("DATABASE_NAME"),
+      user: env("DATABASE_USERNAME"),
+      password: env("DATABASE_PASSWORD"),
       ssl: env.bool("DATABASE_SSL", false) && {
         key: env("DATABASE_SSL_KEY", undefined),
         cert: env("DATABASE_SSL_CERT", undefined),
