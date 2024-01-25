71c88c7147f80ca15df7751d5273d556dc74737d
Mo-mchawrab <mohamad@propertyvalue.ae>
2024-01-25 11:27:58 +0200

    database change

--- config/database.js
+++ config/database.js
@@ -1,24 +1,14 @@
-// strapi-api/config/database.js
+
 module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      connectionString: env("DATABASE_URL"),
-      host: env("DATABASE_HOST"),
-      port: env.int("DATABASE_PORT"),
-      database: env("DATABASE_NAME"),
-      user: env("DATABASE_USERNAME"),
-      password: env("DATABASE_PASSWORD"),
-      ssl: env.bool("DATABASE_SSL", false) && {
-        key: env("DATABASE_SSL_KEY", undefined),
-        cert: env("DATABASE_SSL_CERT", undefined),
-        ca: env("DATABASE_SSL_CA", undefined),
-        capath: env("DATABASE_SSL_CAPATH", undefined),
-        cipher: env("DATABASE_SSL_CIPHER", undefined),
-        rejectUnauthorized: env.bool("DATABASE_SSL_REJECT_UNAUTHORIZED", true),
-      },
-      schema: env("DATABASE_SCHEMA", "public"),
+      host: env("DATABASE_HOST", "127.0.0.1"),
+      port: env.int("DATABASE_PORT", 5432),
+      database: env("DATABASE_NAME", "strapi"),
+      user: env("DATABASE_USERNAME", ""),
+      password: env("DATABASE_PASSWORD", ""),
     },
-    debug: false,
+    useNullAsDefault: true,
   },
 });
