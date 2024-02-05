543ec130117605fd2616cca5704e673b23c5481e
Ubuntu <ubuntu@ip-172-31-3-116.me-central-1.compute.internal>
2024-02-05 10:58:27 +0000

    first main update to main branch

---
+++ src/api/units-dld/routes/units-dld.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * units-dld router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::units-dld.units-dld');
---
+++ src/api/units-dld/services/units-dld.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * units-dld service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::units-dld.units-dld');
